5fd32a1735b2d869b5247aa2b991a6898e0a82fb
Univpier <83532210+Univpier@users.noreply.github.com>
2022-02-10 15:51:17 +0300

    Commit 2

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,17 +8,41 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Test;
+use App\DTO\Response\TestDTO;
 class MainController extends AbstractController
 {
 
     /**
      * @Route("/main", name="main")
      */
-    public function index(ManagerRegistry $doctrine)
+    public function index(Test $test)
     {
-        $repository = $doctrine->getRepository(Test::class);
-        $test= $repository->findAll();
-        dd($test);
+        $dto = new TestDTO();
+
+        $dto->name = $test->getName();
+        $dto->id = $test->getId();
+        $dto->parentid = $test->getParentid();
+        echo json_encode($dto);
+//        $repository = $doctrine->getRepository(Test::class);
+//        $test= $repository->findAll();
+//      var_dump($test[0]->id);
+//        $dto = new TestDTO;
+//        foreach($test as $value){
+//            $dto->name = $test[$value]->name;
+//            $dto->id = $test[$value]->id;
+//            $dto->parentid = $test[$value]->parentid;
+//        }
+//        echo json_encode($dto);
+//        $dto->name = $test->$test["0"]->name;
+//        echo json_encode($dto);
+
+//            $dto->name = $entityTest->getName();
+//            $dto->id = $entityTest->getId();
+//            $dto->parentid = $entityTest->getParentid();
+
+
+
+//        return json_encode($dto);
 //        $req = $test->findAll();
 //        echo json_decode($req);
 //        $arr=[];
